031726af3c453c72840d8e80c8ca98796abc3980
frazerc <frazerchurchill@mac.com>
2023-06-20 13:24:08 +1000

    Initial Commit

---
+++ SimpleAPp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SimpleAPp
+//
+//  Created by Frazer Churchill on 20/6/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimpleAPp/SimpleAPpApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAPpApp.swift
+//  SimpleAPp
+//
+//  Created by Frazer Churchill on 20/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAPpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
